6fd3b089ceefbff71a891bdc27e3013c6bf39ac6
h5tpjbvw77-pixel <h5tpjbvw77@privaterelay.appleid.com>
2026-08-02 20:59:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function send(){
+let msg=document.getElementById("message").value;
 
+let chat=document.getElementById("chat");
+
+chat.innerHTML += 
+"<p>You: "+msg+"</p>";
+
+chat.innerHTML +=
+"<p class='ai'>AI: I received your message 🤖</p>";
+
+document.getElementById("message").value="";
+
+}
+
+
+function settings(){
+
+alert("Settings coming soon ⚙️");
+
+}
